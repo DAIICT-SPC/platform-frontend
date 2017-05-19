dbc345c6ecf4fa0aed24195dc94e87d8683c0799
Sagar Makhija <sagar1309@live.com>
2017-05-19 21:05:04 +0530

    second commit for academic.vue 9.05pm

--- src/container/MyProfile/Academic.vue
+++ src/container/MyProfile/Academic.vue
@@ -30,39 +30,82 @@
             </div> <!-- col 3 ends -->
         </div> <!-- cols ends -->
 
-        <hr id="hracademic">
+        <!-- <hr id="hracademic"> -->
 
-        <!-- table starts -->
-        <div class="result" style="background-color: #f5f5f5;margin-left: 50px; width: 700px;">
-            <h2>Semester Wise Score</h2>
-            <table class="table" style="width: 650px;margin-left: 50px;background-color: #f5f5f5;">
+
+        <div style="overflow-x: auto;">  <!-- table starts -->
+            <table class="table" id="tableacademic">
                 <thead>
+                    <tr>
+                        <th colspan="2" id="tableth">Semester Wise Score</th>
+                    </tr>
                     <tr>
                         <th>Semester</th>
                         <th>Score</th>
-                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th>1</th>
                         <th>8.0</th>
-                        <th></th>
                     </tr>
                     <tr>
                         <th>2</th>
                         <th>6.1</th>
-                        <th></th>
                     </tr>
                     <tr>
                         <th>3</th>
                         <th>8.3</th>
-                        <th></th>
                     </tr>
                 </tbody>
             </table>
         </div> <!-- table ends -->
 
+
+        <!-- previous ed -->
+        <div id="prevedacademic">
+            <div>
+                <h2>Previous Education</h2>
+                <div class="columns">
+                    <!-- col 1 -->
+                    <div class="column is-one-third">
+                        <p>
+                            <b>St. Xavier's High School</b> <br>
+                            Karnataka Board <br>
+                            <small>Senior Secondary Certificate</small> <br>
+                            2010-2011
+                        </p>
+                    </div>
+                    <div class="column"></div> <!-- col 2 -->
+                    <div class="column"> <!-- col 3 -->
+                        <p style="">
+                            72.88%  <br>
+                            Score
+                        </p>
+                    </div>
+                </div>
+
+                <div class="columns">
+                    <!-- col 1 -->
+                    <div class="column is-one-third">
+                        <p>
+                            <b>Vagdevi High School</b> <br>
+                            Karnataka Board <br>
+                            <small>High School Certificate</small> <br>
+                            2008-2009
+                        </p>
+                    </div>
+                    <div class="column"></div> <!-- col 2 -->
+                    <div class="column"> <!-- col 3 -->
+                        <p>
+                            95.04%  <br>
+                            Score
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
 
 
@@ -83,5 +126,21 @@ export default {
 
 #hracademic{
     margin-bottom: 10px; margin-top: 0px; margin-right: 100px; margin-left: 20px; width: 720px;
+}
+
+#result{
+    background-color: #f5f5f5;margin-left: 50px; width: 700px;
+}
+#tableacademic{
+    width: 720px; margin-left: 20px;background-color: #f5f5f5;
+
+}
+#tableth{
+    padding-left: 250px;
+
+}
+#prevedacademic{
+    margin-left: 20px;
+    margin-top: 10px;
 }
 </style>
