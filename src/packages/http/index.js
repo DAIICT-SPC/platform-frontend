b6aa8760a4fa5d76c7fd4e4059d30e45ed7a2714
Kunal Varma <kunalvarma05@gmail.com>
2017-06-02 11:44:37 +0530

    Authorization Token Request Interceptor

--- src/packages/http/index.js
+++ src/packages/http/index.js
@@ -2,9 +2,25 @@ import axios from 'axios';
 import Auth from '@/packages/auth/Auth';
 import config from '@/config';
 
-export default axios.create({
-  baseURL: config.get('apiUrl'),
-  headers: {
-    Authorization: 'Bearer ' + Auth.getToken()
-  }
+
+let axiosInstance = axios.create({
+  baseURL: config.get('apiUrl')
 });
+
+// This interceptor, adds the Authorization Token to the
+// Request Header, if the User is logged in.
+axiosInstance.interceptors.request.use(function(axiosConfig) {
+  // The User is logged in,
+  // thus, the token is available.
+  if (Auth.isAuthenticated()) {
+    // Add token to the Authorization Header of the Request
+    axiosConfig.headers.common["Authorization"] = "Bearer " + Auth.getToken();
+  }
+
+  return axiosConfig;
+}, function(error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+export default axiosInstance;
